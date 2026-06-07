b1d15ea21905758592d2f4b5c4e3aa3f1c93b82d
Simon Puccio <simon.puccio@epitech.eu>
2026-06-07 22:27:42 +0200

    feat: weather icons module and update weather icon retrieval

--- src/weather/sync_weather.rs
+++ src/weather/sync_weather.rs
@@ -1,4 +1,5 @@
 use crate::network::http::http_get;
+use crate::weather::icons::{*};
 
 use anyhow::{Result};
 
@@ -13,22 +14,17 @@ pub struct CurrentWeather {
     pub weathercode: u8,
 }
 
-pub fn get_weather_icon(code: u8) -> &'static str {
+pub fn get_weather_icon(code: u8) -> &'static [u8] {
     match code {
-        0 => "☀️", // Clear sky
-        1 | 2 => "🌤️", // Mainly clear, partly cloudy
-        3 => "☁️", // Overcast
-        45 | 48 => "🌫️", // Fog and depositing rime fog
-        51 | 53 | 55 => "🌦️", // Drizzle: Light, moderate, and dense intensity
-        56 | 57 => "🌨️", // Freezing Drizzle: Light and dense intensity
-        61 | 63 | 65 => "🌧️", // Rain: Slight, moderate and heavy intensity
-        66 | 67 => "🧊", // Freezing Rain: Light and heavy intensity
-        71 | 73 | 75 => "❄️", // Snow fall: Slight, moderate, and heavy intensity
-        77 => "🌨️", // Snow grains
-        80 | 81 | 82 => "🌧️", // Rain showers: Slight, moderate, and violent
-        85 | 86 => "🌨️", // Snow showers slight and heavy
-        95 | 96 | 99 => "⛈️", // Thunderstorm
-        _ => "❓", // Unknown weather code
+        0 => &ICON_SUN_LARGE, // Clear sky
+        1 | 2 => &ICON_CLOUD_SUNNY_LARGE, // Mainly clear, partly cloudy
+        3 => &ICON_CLOUD_LARGE, // Overcast
+        45 | 48 => &ICON_FOG_LARGE, // Fog and depositing rime fog
+        51 | 53 | 55 | 61 | 63 | 65 | 80 | 81 | 82 => &ICON_RAIN_LARGE, // Drizzle: Light, moderate, and dense intensity
+        56 | 57 | 66 | 67 => &ICON_SLEET_LARGE, // Freezing Rain: Light and heavy intensity
+        71 | 73 | 75 | 77 | 85 | 86 => &ICON_SNOW_LARGE, // Snow fall: Slight, moderate, and heavy intensity
+        95 | 96 | 99 => &ICON_THUNDERSTORM_LARGE, // Thunderstorm
+        _ => &ICON_UNKNOWN_LARGE, // Unknown weather code
     }
 }
 
